af7ab2b5ca26f8451a1aff08f5af4b611940b4fa
William Li <githuberr@outlook.com>
2018-05-18 23:34:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,11 @@
 # Write your code here.
 
-def badge_make(name)
+def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(arr)
   message = Array.new
-  arr.each {|item| message << badge_make(item)}
+  arr.each {|item| message << badge_maker(item)}
   message
 end
